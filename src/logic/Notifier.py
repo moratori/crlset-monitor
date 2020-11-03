24906828d177b68eb8d74f212838c1cd569a0ff7
moratori <hoko1993@yahoo.co.jp>
2020-11-03 20:49:44 +0900

    twitterへの投稿を実装

--- src/logic/Notifier.py
+++ src/logic/Notifier.py
@@ -3,6 +3,7 @@
 import requests
 import json
 import time
+import tweepy
 from logging import getLogger
 from typing import List
 from typing import Tuple
@@ -17,6 +18,12 @@ class Notifier:
 
         self.slack_url = cnfs.notify.slack
         self.notify_wait = cnfs.notify.wait
+
+        self.consumer_key = cnfs.notify.twitter_consumer_key
+        self.consumer_secret = cnfs.notify.twitter_consumer_secret
+        self.access_token = cnfs.notify.twitter_access_token
+        self.access_token_secret = \
+            cnfs.notify.twitter_access_token_secret
 
         self.notify_target = notify_target
 
@@ -44,12 +51,21 @@ class Notifier:
             json.dumps(dict(text=message)),
             headers={'Content-Type': 'application/json'})
 
-        return result.status_code
+        LOGGER.info("slack post http status code: %d" % result.status_code)
 
     def _notify_twitter(self, caname: str, added: List[str]):
         message = self._make_notify_twitter_message(caname, added)
 
-        return 200
+        auth = tweepy.OAuthHandler(self.consumer_key,
+                                   self.consumer_secret)
+
+        auth.set_access_token(self.access_token,
+                              self.access_token_secret)
+
+        api = tweepy.API(auth)
+        ret = api.update_status(message)
+
+        LOGGER.debug("tweepy update_status: %s" % str(ret))
 
     def notify(self):
 
@@ -62,4 +78,3 @@ class Notifier:
                 except Exception as ex:
                     LOGGER.error("error while notifying: %s" % str(ex))
             time.sleep(self.notify_wait)
-
