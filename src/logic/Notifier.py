6b43b91170f2ce253e4d9bd3beb8b981d10662d2
moratori <hoko1993@yahoo.co.jp>
2020-12-05 14:32:29 +0900

    crt.shの詳細リンクをリプライで送るように修正

--- src/logic/Notifier.py
+++ src/logic/Notifier.py
@@ -7,6 +7,7 @@ import tweepy
 from logging import getLogger
 from typing import List
 from typing import Tuple
+from typing import Optional
 
 LOGGER = getLogger(__name__)
 
@@ -18,6 +19,8 @@ class Notifier:
 
         self.slack_url = cnfs.notify.slack
         self.notify_wait = cnfs.notify.wait
+        self.crtsh_base_url = cnfs.notify.crtsh_base_url
+        self.myaccount = cnfs.notify.twitter_account
 
         self.consumer_key = cnfs.notify.twitter_consumer_key
         self.consumer_secret = cnfs.notify.twitter_consumer_secret
@@ -45,9 +48,7 @@ class Notifier:
 
         return tmp_mes + serials
 
-    def _notify_slack(self, caname: str, added: List[str]):
-        message = self._make_notify_slack_message(caname, added)
-
+    def _notify_slack(self, message: str):
         result = requests.post(
             self.slack_url,
             json.dumps(dict(text=message)),
@@ -55,9 +56,10 @@ class Notifier:
 
         LOGGER.info("slack post http status code: %d" % result.status_code)
 
-    def _notify_twitter(self, caname: str, added: List[str]):
-        message = self._make_notify_twitter_message(caname, added)
+        return result
 
+    def _notify_twitter(self, message: str,
+                        in_reply_to_status_id: Optional[int]):
         auth = tweepy.OAuthHandler(self.consumer_key,
                                    self.consumer_secret)
 
@@ -65,18 +67,49 @@ class Notifier:
                               self.access_token_secret)
 
         api = tweepy.API(auth)
-        ret = api.update_status(message)
+        if in_reply_to_status_id is None:
+            ret = api.update_status(message)
+        else:
+            ret = api.update_status(message,
+                                    in_reply_to_status_id=in_reply_to_status_id)
 
         LOGGER.debug("tweepy update_status: %s" % str(ret))
 
+        return ret
+
+    def _replay_detailed_cert_info(self, org_tweet, added: List[str]):
+
+        for serial in added:
+            try:
+                crtsh_url = self.crtsh_base_url % serial
+                message = self.myaccount + " " + crtsh_url
+                self._notify_twitter(message, org_tweet.id)
+            except Exception as ex:
+                LOGGER.warn("error while replay to original tweet: %s" %
+                            str(ex))
+            time.sleep(self.notify_wait)
+
+    def _notify_added_serials(self, caname: str, added: List[str]):
+        try:
+            twitter_message = self._make_notify_twitter_message(caname, added)
+            slack_message = self._make_notify_slack_message(caname, added)
+
+            self._notify_slack(slack_message)
+            ret_twitter = self._notify_twitter(twitter_message, None)
+        except Exception as ex:
+            LOGGER.error("error while notifying: %s" % str(ex))
+
+        self._replay_detailed_cert_info(ret_twitter, added)
+
+    def _notify_reduced_serials(self, caname: str, added: List[str]):
+        # reduced は一旦通知対象にはしない
+        pass
+
     def notify(self):
 
-        # reduced は一旦通知対象にはしない
         for (caname, added, reduced) in self.notify_target:
             if len(added) != 0:
-                try:
-                    self._notify_slack(caname, added)
-                    self._notify_twitter(caname, added)
-                except Exception as ex:
-                    LOGGER.error("error while notifying: %s" % str(ex))
+                self._notify_added_serials(caname, added)
+            if len(reduced) != 0:
+                self._notify_reduced_serials(caname, reduced)
             time.sleep(self.notify_wait)
